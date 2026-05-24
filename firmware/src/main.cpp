7d68f59d1e610dd3a41158527d55d45b2325891c
Anvarys <105349498+Anvarys@users.noreply.github.com>
2026-05-24 18:05:42 +0200

    added RTC support to firmware & now it displays it

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,10 +3,12 @@
 #include <U8g2lib.h>
 #include <MUIU8g2.h>
 #include <Adafruit_MPR121.h>
+#include <DS3231.h>
 
 #ifndef _BV
 #define _BV(bit) (1 << (bit)) 
 #endif
+
 
 #define I2C0_SDA 4
 #define I2C0_SCL 5
@@ -21,13 +23,16 @@ Adafruit_MPR121 mpr121_0 = Adafruit_MPR121();
 Adafruit_MPR121 mpr121_1 = Adafruit_MPR121();
 Adafruit_MPR121 mpr121_2 = Adafruit_MPR121();
 
-
 U8G2_SSD1309_128X64_NONAME0_F_HW_I2C display(
   U8G2_R2, // 180 degrees rotation because the screen is flipped
   U8X8_PIN_NONE,
   I2C0_SCL,
   I2C0_SDA
 );
+
+DS3231 RTC;
+bool h12;
+bool hPM;
 
 volatile bool irqFired = false;
 
@@ -40,15 +45,24 @@ enum ControlButton {
 bool controlsClicked = false;
 ControlButton controlButtonClicked = SQUARE;
 
+uint64_t lastButtonActivity = 0;
+uint64_t timeInactive = 60 * 1000; // ms
+
+uint64_t lastTimeUpdate = 0;
+
 void onIRQ() {
   irqFired = true;
 }
 
 void setup() {
-  display.begin();
-
   Wire.begin();
 
+  // display
+
+  display.begin();
+  display.clearBuffer();
+
+  // capacitive touch ICs
   mpr121_0.begin(0x5A, &Wire, MPR121_TOUCH_THRESHOLD_DEFAULT, MPR121_RELEASE_THRESHOLD_DEFAULT, MPR121_AUTOCONFIG);
   mpr121_1.begin(0x5B, &Wire, MPR121_TOUCH_THRESHOLD_DEFAULT, MPR121_RELEASE_THRESHOLD_DEFAULT, MPR121_AUTOCONFIG);
   mpr121_2.begin(0x5C, &Wire, MPR121_TOUCH_THRESHOLD_DEFAULT, MPR121_RELEASE_THRESHOLD_DEFAULT, MPR121_AUTOCONFIG);
@@ -60,6 +74,14 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(MPR121_0_IRQ), onIRQ, FALLING);
   attachInterrupt(digitalPinToInterrupt(MPR121_1_IRQ), onIRQ, FALLING);
   attachInterrupt(digitalPinToInterrupt(MPR121_2_IRQ), onIRQ, FALLING);
+
+  // RTC
+
+  RTC.setClockMode(false);
+
+  if (!RTC.oscillatorCheck()) {
+    RTC.setEpoch(1779637745); // !!! modify this to your current time
+  }
 }
 
 
@@ -90,6 +112,7 @@ void updateMPR121s() {
   if (maxDeltaId != -1) {
     circleClicked = true;
     circleClickedId = maxDeltaId;
+    lastButtonActivity = millis();
   } else {
     circleClicked = false;
   }
@@ -111,10 +134,22 @@ void updateMPR121s() {
   if (maxDeltaId != -1) {
     controlsClicked = true;
     controlButtonClicked = (ControlButton) maxDeltaId;
+    lastButtonActivity = millis();
   }
+}
+
+void updateTimeOnDisplay() {
+  if (!((millis() - lastTimeUpdate >= 1000 && millis() - lastButtonActivity >= timeInactive))) return;
+  lastTimeUpdate = millis();
+
+  display.clearBuffer();
+  display.setFont(u8g2_font_logisoso38_tn);
+  display.drawStr(20, 10, RTC.getHour(h12, hPM) + ":" + RTC.getMinute());
+  display.sendBuffer();
 }
 
 
 void loop() {
   updateMPR121s();
+  updateTimeOnDisplay();
 }
